fb3c0d9b79b8bec7dc92889d0466339530ece24c
Pavan <pavanpandya016@gmail.com>
2026-03-10 16:12:32 +0530

    mongoo atlas connection

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import connectDB from "./db/connect.js";
+import 'dotenv/config';
+
+connectDB()
+
+// import express from "express"
+// const app = express()
+
+// ( async ()=>{
+//     try {
+//         await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+//         app.on("error", (error)=>{
+//             console.log("Error", error);
+//             throw error
+//         })
+
+//         app.listen(process.env.PORT, ()=>{
+//             console.log(`app is listening on port ${process.env.PORT}`);
+//         })
+
+//     } catch (error) {
+//         console.error("Error: ", error)
+//         throw error
+//     }
+// })()
