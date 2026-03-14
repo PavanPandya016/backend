862b6f36d01af291ae8975d4c75e88ebab586c83
Pavan <pavanpandya016@gmail.com>
2026-03-14 14:22:22 +0530

    file upload config

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,36 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null;
+    const uploadResult = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+    console.log("file is uploaded on cloudinary: ", uploadResult.url);
+    return uploadResult;
+  } catch (error) {
+    fs.unlinkSync(localFilePath) // Remove the locally saved temp file as upload operation got failed
+    console.log("File Upload Error: ", error);
+    return null
+  }
+};
+
+export { uploadOnCloudinary };
+
+// const uploadResult = await cloudinary.uploader
+//   .upload(
+//     "https://res.cloudinary.com/demo/image/upload/getting-started/shoes.jpg",
+//     {
+//       public_id: "shoes",
+//     }
+//   )
+//   .catch((error) => {
+//     console.log(error);
+//   });
